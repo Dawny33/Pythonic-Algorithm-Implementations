246a5271b84cc82261515c69620c7a1f63b5e569
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-06-18 18:56:59 +0530

    updated error in any_to_bin

--- Abstract Data Types/Stack/Anything_to_Binary.py
+++ Abstract Data Types/Stack/Anything_to_Binary.py
@@ -1,15 +1,18 @@
 
-
 def any_to_bin(number,base):
+    digits = "0123456789ABCDEF"
 	rem = Stack()
 
 	while number > 0:
 		reminder = number % base
-		rem = rem.push(reminder)
-		number //= base
+		rem.push(reminder)
+		number = number // base
 
 	binary = ""
 	while not rem.isEmpty():
-		binary = binary + str(rem.pop())
+		binary = binary + digits[rem.pop()]
 
 	return binary
+
+
+print(any_to_bin(42,8))

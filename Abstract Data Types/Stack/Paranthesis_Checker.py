8b800733c5eedbd8ce6dd6291591314cbbf409e2
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-06-18 15:38:44 +0530

    updated the Stack example
    Paranthesis checker

---
+++ Abstract Data Types/Stack/Paranthesis_Checker.py
@@ -0,0 +1,25 @@
+#Please import the Stack class from the Implementation file.
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+#print(parChecker('((()))'))
+#print(parChecker('(()'))

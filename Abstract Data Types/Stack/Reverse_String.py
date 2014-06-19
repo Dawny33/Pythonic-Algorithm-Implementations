1c2f8276e5096e1d50e089958ea80cf20f7fe254
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-06-19 19:41:50 +0530

    Reversing a string using Stack
    An example.
    (Geeks for geeks question)

---
+++ Abstract Data Types/Stack/Reverse_String.py
@@ -0,0 +1,35 @@
+class Stack:
+
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.size() == 0
+
+	def push(self,item):
+		return self.items.insert(0,item)
+
+	def pop(self):
+		return self.items.pop(0)
+
+	def peek(self):
+		return self.items[0]
+
+	def size(self):
+		return len(self.items)		
+
+def Str_rev(target):
+
+	stack_obj = Stack()
+	for char in target:
+	      stack_obj.push(char)
+
+	 while not stack_obj.isEmpty():
+            print(stack_obj.pop())
+
+#An alternative implementation would be:
+	for i in range(stack_obj.size()):
+		print stack_obj.pop()
+	
+
+print(Str_rev("ABC"))

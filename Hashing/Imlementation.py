c0a8a5fe5073972a387a014bc3a83647209b31e3
Dawny33 <rajrohit@iitj.ac.in>
2014-08-12 12:52:13 +0530

    Fixed bugs

--- Hashing/Imlementation.py
+++ Hashing/Imlementation.py
@@ -6,10 +6,10 @@ class HashTable:
     def __init__(self):
         self.size = 11
         self.slots = [None] * self.size
-        self.size = [None] * self.size
+        self.data = [None] * self.size
 
 #The put(insert) module.
-    def put(self,key,value):
+    def put(self,key,data):
         hashvalue = self.hashfun(key, len(self.slots))
 
         if self.slots == None:
@@ -37,3 +37,43 @@ class HashTable:
     def rehash(slf, oldhash, size):
         return (oldhash+1)%size
                     
+#The get(retrieve) module.
+    def get(self,key):
+          startslot = self.hashfunction(key,len(self.slots))
+
+          data = None
+          stop = False
+          found = False
+          position = startslot
+          while self.slots[position] != None and  not found and not stop:
+             if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+             else:
+                position=self.rehash(position,len(self.slots))
+                if position == startslot:
+                    stop = True
+          return data
+
+#The get item constructor module.
+    def __getitem__(self,key):
+        return self.get(key)
+
+#The set item constructor module
+    def __setitem__(self,key,data):
+        self.put(key,data)
+
+
+H = HashTable()
+H[54] = "cat"
+H[26] = "dog"
+H[93] = "lion"
+H[17] = "tiger"
+H[77] = "bird"
+H[31] = "cow"
+H[44] = "goat"
+H[55] = "pig"
+H[20] = "chicken"
+
+print H.slots
+print H.data
